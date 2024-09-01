10a3eb28e96c4590d6f3bed5b02c2e622691cf29
tteuw <wayne@wayne.wayne>
2024-09-01 20:35:27 -0300

    changed the code

--- renderer.js
+++ renderer.js
@@ -1,17 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const playBtn = document.getElementById('playBtn');
-    playBtn.addEventListener('click', () => {
-        alert('Playing Breaking Bad');
-    });
-
-    const menuBtn = document.getElementById('menuBtn');
-    menuBtn.addEventListener('click', () => {
-        alert('Menu clicked');
-    });
-
-    const settingsBtn = document.getElementById('settingsBtn');
-    settingsBtn.addEventListener('click', () => {
-        alert('Settings clicked');
-    });
+// You can add interactivity using JavaScript here
+document.getElementById('playBtn').addEventListener('click', function() {
+    // Play the video or perform some action
+    console.log('Play button clicked');
 });
-
