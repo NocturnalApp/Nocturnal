d277c318ea94e6f20e5ed12ea909208c1e1f8aaa
tteuw00111111 <tteuw021@proton.me>
2024-09-07 20:16:47 -0300

    Update navbar symbol with React-Icons and updated structure

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,29 @@
 import styles from './Navbar.module.css'
 import nocturnal_txt from './../../assets/noctural_txt.png'
-import settings_icon from './../../assets/settings_icon.png'
+/*import settings_icon from './../../assets/settings_icon.png'
 import hamburger_menu from './../../assets/hamburger-menu.png'
-import search_icon from './../../assets/search_icon.png'
+import search_icon from './../../assets/search_icon.png' */
+import { CiMenuBurger } from "react-icons/ci";
+import { CiSettings } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
+
 
 const Navbar = () => {
     return <nav className={styles.Navbar}>
     <a href=""><img className={styles.logo} src={nocturnal_txt} alt="Nocturnal" /></a>
+
     <ul>
         <li>
-            <a href=""><img className={styles.search} src={search_icon} alt="menu" /></a>
-        </li>       
+            <a href=""><CiSearch size={25}/></a>
+        </li>  
+
         <li>
-            <a href=""><img className={styles.hamburger} src={hamburger_menu} alt="menu" /></a>
+            <a href=""><CiMenuBurger size={23}/></a>
         </li>
         <li>
-            <a href=""><img className={styles.settings} src={settings_icon} alt="settings" /></a>
+            <a href=""> <CiSettings size={25}/> </a>
         </li>
     </ul>
-
-
     </nav>
 }
 export default Navbar
