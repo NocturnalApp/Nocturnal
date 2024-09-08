49af19c1364ff7be0ba972619ca277bc3cd9098e
tteuw00111111 <tteuw021@proton.me>
2024-09-08 14:31:34 -0300

    Remove Navbar.module.css, styles added directly into Navbar.tsx, hovering effect over buttons and log, and sticky navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,47 @@
-import styles from './Navbar.module.css';
 import nocturnal_txt from './../../assets/noctural_txt.png';
 import { CiMenuBurger, CiSearch } from "react-icons/ci";
 import { GiSettingsKnobs } from "react-icons/gi";
 
 const Navbar = () => {
   return (
-    <nav className={styles.Navbar}>
+    <nav
+      className="flex items-center justify-between sticky top-0 p-2 bg-gradient-to-r from-[#070707] to-[#6D6D6D] bg-opacity-10 shadow-md backdrop-blur-md"
+      style={{
+        background: 'linear-gradient(to right, #070707, rgba(109, 109, 109, 0.15))',
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        boxShadow: '0px 4px 12px rgba(7, 7, 7, 0.5)',
+        backdropFilter: 'blur(5px)',
+        height: '48px', // Adjust height as needed
+      }}
+    >
       <a href="">
-        <img className="h-8" src={nocturnal_txt} alt="Nocturnal" />
+        <img
+          className="h-6 transition-transform transform hover:scale-105"
+          src={nocturnal_txt}
+          alt="Nocturnal"
+        />
       </a>
-      <ul className="flex space-x-4">
+      <ul className="flex space-x-4 items-center">
         <li>
-          <a href="" className={styles.icon}>
-            <CiSearch size={25} />
+          <CiSearch
+            size={20}
+            className="text-white transition-colors duration-300 hover:text-gray-400"
+          />
+        </li>
+        <li>
+          <a
+            href=""
+            className="text-white transition-colors duration-300 hover:text-gray-400"
+          >
+            <CiMenuBurger size={20} />
           </a>
         </li>
         <li>
-          <a href="" className={styles.icon}>
-            <CiMenuBurger size={25} />
-          </a>
-        </li>
-        <li>
-          <a href="" className={styles.icon}>
-            <GiSettingsKnobs size={25} />
+          <a
+            href=""
+            className="text-white transition-colors duration-300 hover:text-gray-400"
+          >
+            <GiSettingsKnobs size={20} />
           </a>
         </li>
       </ul>
@@ -31,3 +50,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
