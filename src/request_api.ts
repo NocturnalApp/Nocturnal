1dfda4691c258c81db66ae346826d61789ec00e1
chopin014 <chopin14@tuta.io>
2024-09-08 18:27:53 -0300

    first implementation of the tmdb requests

---
+++ src/request_api.ts
@@ -0,0 +1,11 @@
+const key = '50674bd62cf713eedbc7bd60288e0dbd'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/movie/week?api_key=${key}&language=en-US&page=1`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
---
+++ src/requests.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import {useEffect, useState} from 'react'
+import request from './request_api'
+
+
+const Request = () => {
+
+    const [movies, setMovies] = useState([])
+
+    const movie = movies.length > 0 ? movies[Math.floor(Math.random() * movies.length)] : null;
+    
+
+    useEffect(()=>{
+        axios.get(request.requestTrending).then((response)=>{
+            setMovies(response.data.results)
+        })
+
+    },[])
+    console.log(movie);
+    
+    return(
+        <div>Request</div>
+    )
+        
+    
+
+}
+export default Request
