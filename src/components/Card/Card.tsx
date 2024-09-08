204b6d5c298d7d7b58da0c3af186289b992a9055
tteuw00111111 <tteuw021@proton.me>
2024-09-08 16:50:08 -0300

    Add tittle, category buttons and synopsis for homepage

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,35 +4,36 @@ import { FaPlay } from 'react-icons/fa'; // Import the play icon
 
 const Card = () => {
   return (
-    <div className="relative flex h-screen">
+    <div className="relative flex h-screen p-8">
       {/* Card Component */}
       <div className="relative w-[50%] h-full">
-        <div className="absolute top-[35%] left-1/2 transform -translate-x-1/2 -translate-y-[25%] w-[400px] h-[600px] md:w-[18vw] md:h-[50vh] max-w-[400px] max-h-[650px] 
-                        bg-cover bg-center rounded-[100px] drop-shadow-2xl transition-transform duration-300 ease-in-out hover:scale-110"
-             style={{ backgroundImage: `url(${br2})` }}>
+        <div
+          className="absolute top-[35%] left-[75%] transform -translate-x-1/2 -translate-y-[25%] w-[400px] h-[600px] md:w-[18vw] md:h-[50vh] max-w-[400px] max-h-[650px] 
+                     bg-cover bg-center rounded-[100px] drop-shadow-2xl transition-transform duration-300 ease-in-out hover:scale-110"
+          style={{ backgroundImage: `url(${br2})` }}>
           <div className="absolute inset-0 flex items-center justify-center">
             <FaPlay 
-                className="text-white text-7xl rounded-[50px]cursor-pointer opacity-80 hover:opacity-100 drop-shadow-lg transition duration-300 ease-in-out"/>
+              className="text-white text-7xl rounded-[50px]cursor-pointer opacity-80 hover:opacity-100 drop-shadow-lg transition duration-300 ease-in-out"/>
           </div>
         </div>
       </div>
 
-     {/* Sidebar with Title, Buttons, and Synopsis
-      <div className="absolute w-[50%] bottom-10 top-96 left-1/4">
-        <h1 className="text-4xl font-bold text-white mb-4">Breaking Bad</h1>
+      {/* Sidebar with Title, Buttons, and Synopsis */}
+      <div className="absolute top-1/2 left-[48%] transform -translate-y-1/2">
+        <h1 className="text-4xl lg:text-5xl font-bold text-white mb-4">Breaking Bad</h1>
         <div className="flex space-x-4 mb-4">
-          <button className="px-4 py-2 bg-transparent border border-white text-white rounded-md backdrop-blur-md transition duration-300 ease-in-out hover:bg-white hover:text-black">
+          <button className="px-6 py-2  border border-white text-white rounded-[50px] bg-white/30 backdrop-blur-2xl transition duration-300 ease-in-out hover:bg-white hover:text-black">
             Category 1
           </button>
-          <button className="px-4 py-2 bg-transparent border border-white text-white rounded-md backdrop-blur-md transition duration-300 ease-in-out hover:bg-white hover:text-black">
+          <button className="px-6 py-2 border border-white text-white rounded-[50px] bg-white/30 backdrop-blur-2xl transition duration-300 ease-in-out hover:bg-white hover:text-black">
             Category 2
           </button>
         </div>
-        <p className="text-white text-lg">
-          Breaking Bad follows Walter White, a high school chemistry teacher turned methamphetamine manufacturer, as he navigates the dangerous world of drug trafficking. Faced with a terminal cancer diagnosis, Walter partners with former student Jesse Pinkman to secure his family’s future while battling moral dilemmas and enemies both internal and external.
+        <p className="text-white text-lg lg:text-xl max-w-lg">
+          Breaking Bad follows Walter White, a high school chemistry teacher turned methamphetamine manufacturer, as he navigates the dangerous world of drug trafficking.
         </p>
-      </div> */}
-      </div> 
+      </div>
+    </div>
   );
 }
 
