c7d0456d2d751ea8e1269f709c191a55c9dd1055
tteuw00111111 <tteuw021@proton.me>
2024-09-08 17:25:09 -0300

    Config tailwind for text-shadow

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,7 @@ const Card = () => {
       {/* Card Component */}
       <div className="relative w-[50%] h-full">
         <div
-          className="absolute top-[35%] left-[75%] transform -translate-x-1/2 -translate-y-[25%] w-[400px] h-[600px] md:w-[18vw] md:h-[50vh] max-w-[400px] max-h-[650px] 
+          className="shadow-2xl absolute top-[35%] left-[75%] transform -translate-x-1/2 -translate-y-[25%] w-[400px] h-[600px] md:w-[18vw] md:h-[50vh] max-w-[400px] max-h-[650px] 
                      bg-cover bg-center rounded-[100px] drop-shadow-2xl transition-transform duration-300 ease-in-out hover:scale-110"
           style={{ backgroundImage: `url(${br2})` }}>
           <div className="absolute inset-0 flex items-center justify-center">
@@ -20,16 +20,16 @@ const Card = () => {
 
       {/* Sidebar with Title, Buttons, and Synopsis */}
       <div className="absolute top-[43%] left-[48%] transform -translate-y-1/2">
-        <h1 className="text-4xl lg:text-6xl font-bold text-white mb-4">Breaking Bad</h1>
+        <h1 className="text-shadow-md font-robot text-6xl lg:text-6xl font-bold text-white mb-4">Breaking Bad</h1>
         <div className="flex space-x-4 mb-4">
-          <button className="px-6 py-2  border border-white text-white rounded-[50px] bg-white/30 backdrop-blur-2xl transition duration-300 ease-in-out hover:bg-white hover:text-black hover:scale-105">
+          <button className="shadow-md	 font-roboto px-6 py-2  border border-white text-white rounded-[50px] bg-white/30 backdrop-blur-2xl transition duration-300 ease-in-out hover:bg-white hover:text-black hover:scale-105">
             Category 1
           </button>
-          <button className="px-6 py-2 border border-white text-white rounded-[50px] bg-white/30 backdrop-blur-2xl transition duration-300 ease-in-out hover:bg-white hover:text-black hover:scale-105">
+          <button className="shadow-md font-roboto px-6 py-2 border border-white text-white rounded-[50px] bg-white/30 backdrop-blur-2xl transition duration-300 ease-in-out hover:bg-white hover:text-black hover:scale-105">
             Category 2
           </button>
         </div>
-        <p className="text-white text-lg lg:text-xl max-w-lg">
+        <p className=" font-roboto text-white text-lg lg:text-sm max-w-lg">
           Breaking Bad follows Walter White, a high school chemistry teacher turned methamphetamine manufacturer, as he navigates the dangerous world of drug trafficking.
         </p>
       </div>
