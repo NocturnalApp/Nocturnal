cb3e403f3e790497bb7c489539c256565354f7e7
chopin014 <chopin14@tuta.io>
2024-09-15 21:49:29 -0300

    add Documentaries to the catalog

--- src/request_api.ts
+++ src/request_api.ts
@@ -8,6 +8,8 @@ const requests = {
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
     requestAnimes: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&page=1&with_genres=16&with_keywords=210024`,
     requestTVShows: `https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=99`
+
   };
 
   export default requests
